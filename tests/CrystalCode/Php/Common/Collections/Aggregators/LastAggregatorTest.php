b4c1708daf5001c9590560d5a6fbd03f673e00ff
Dan Lugg <dan.lugg@gmail.com>
2018-10-03 22:22:29 -0400

    Added implementation for LastKeyAggregator. Added more tests.

--- tests/CrystalCode/Php/Common/Collections/Aggregators/LastAggregatorTest.php
+++ tests/CrystalCode/Php/Common/Collections/Aggregators/LastAggregatorTest.php
@@ -5,15 +5,15 @@ namespace CrystalCode\Php\Common\Collections\Aggregators;
 use CrystalCode\Php\Common\Collections\Collection;
 use PHPUnit\Framework\TestCase;
 
-class FirstAggregatorTest extends TestCase
+class LastAggregatorTest extends TestCase
 {
 
     public function testSimple()
     {
         t1: {
             $result = Collection::create([1, 2, 3, 4, 5])
-            ->aggregateWith(new FirstAggregator(function ($value) {
-                return $value >= 3;
+            ->aggregateWith(new LastAggregator(function ($value) {
+                return $value <= 3;
             }));
 
             $this->assertEquals(3, $result);
@@ -21,26 +21,26 @@ class FirstAggregatorTest extends TestCase
 
         t2: {
             $result = Collection::create([1, 1, 1, 1, 1])
-            ->aggregateWith(new FirstAggregator(function ($value) {
-                return $value >= 3;
+            ->aggregateWith(new LastAggregator(function ($value) {
+                return $value <= 3;
+            }));
+
+            $this->assertEquals(1, $result);
+        }
+
+        t3: {
+            $result = Collection::create([5, 5, 5, 5, 5])
+            ->aggregateWith(new LastAggregator(function ($value) {
+                return $value <= 3;
             }));
 
             $this->assertNull($result);
         }
 
-        t3: {
-            $result = Collection::create([5, 5, 5, 5, 5])
-            ->aggregateWith(new FirstAggregator(function ($value) {
-                return $value >= 3;
-            }));
-
-            $this->assertEquals(5, $result);
-        }
-
         t4: {
             $result = Collection::create([])
-            ->aggregateWith(new FirstAggregator(function ($value) {
-                return $value >= 3;
+            ->aggregateWith(new LastAggregator(function ($value) {
+                return $value <= 3;
             }));
 
             $this->assertNull($result);
---
+++ tests/CrystalCode/Php/Common/Collections/Aggregators/LastKeyAggregatorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CrystalCode\Php\Common\Collections\Aggregators;
+
+use CrystalCode\Php\Common\Collections\Collection;
+use PHPUnit\Framework\TestCase;
+
+class LastKeyAggregatorTest extends TestCase
+{
+
+    public function testSimple()
+    {
+        t1: {
+            $result = Collection::create(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5])
+            ->aggregateWith(new LastKeyAggregator(function ($value) {
+                return $value >= 3;
+            }));
+
+            $this->assertEquals('e', $result);
+        }
+
+        t2: {
+            $result = Collection::create(['a' => 1, 'b' => 1, 'c' => 1, 'd' => 1, 'e' => 1])
+            ->aggregateWith(new LastKeyAggregator(function ($value) {
+                return $value >= 3;
+            }));
+
+            $this->assertNull($result);
+        }
+
+        t3: {
+            $result = Collection::create(['a' => 5, 'b' => 5, 'c' => 5, 'd' => 5, 'e' => 5])
+            ->aggregateWith(new LastKeyAggregator(function ($value) {
+                return $value >= 3;
+            }));
+
+            $this->assertEquals('e', $result);
+        }
+
+        t4: {
+            $result = Collection::create([])
+            ->aggregateWith(new LastKeyAggregator(function ($value) {
+                return $value >= 3;
+            }));
+
+            $this->assertNull($result);
+        }
+    }
+
+}
